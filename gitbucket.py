25a60fc369fcf23a8b6049b080a28e30548227c0
williaammolina <wmolina98@gmail.com>
2019-08-27 20:48:09 -0500

    Remove unnecesary headers

--- gitbucket.py
+++ gitbucket.py
@@ -4,9 +4,7 @@ def obtener_ramas():
     url = "http://192.168.26.64:8080/api/v3/repos/TheWildCats/Selenium_Updated/branches"
     payload = ""
     headers = {
-        'Authorization': "Basic SGJ1ZWx2YXM6bmFjaW9uYWw4NTIw",
-        'cache-control': "no-cache",
-        'Postman-Token': "37512018-3b5e-4551-ac80-34a4927a0dd2"
+        'Authorization': "Basic SGJ1ZWx2YXM6bmFjaW9uYWw4NTIw"
         }
 
     response = requests.request("GET", url, data=payload, headers=headers)
